860bb8f84d304a7ad14da38eb2f63ffd50731d19
micazev <micazev@protonmail.com>
2019-06-12 17:23:48 -0300

    deletei tudo

--- README.md
+++
@@ -1,2 +0,0 @@
-# ONDE COMPRAR?
-Projeto individual para avaliação de conhecimentos adquiridos no primeiro semestre no curso técnico de análise de sistemas na Bandtec.
--- repositorio.sql
+++
@@ -1,9 +0,0 @@
- create table Loja (
-     lojaId int PRIMARY KEY auto_increment,
-     nome varchar(30),
-     acesso url,
-     categoria VARCHAR(30),
-     responsa VARCHAR(30)
- );
-
- 
